848d27afc4af9b1a296a36738f198402b624dffd
eloy <eloysandoval10@hotmail.com>
2025-02-09 14:35:09 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { HashRouter as Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
